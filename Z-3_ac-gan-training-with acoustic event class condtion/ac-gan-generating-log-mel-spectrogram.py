9a777f38f5759da7649c79f71a639fac75a2fcee
xianjunxia <xianjun.xia@outlook.com>
2018-09-06 09:59:13 +0800

    Update ac-gan-generating-log-mel-spectrogram.py

--- Z-3_ac-gan-training-with acoustic event class condtion/ac-gan-generating-log-mel-spectrogram.py
+++ Z-3_ac-gan-training-with acoustic event class condtion/ac-gan-generating-log-mel-spectrogram.py
@@ -113,8 +113,8 @@ def GetTrainData(GenerateType):
 
 def modified_binary_crossentropy(target, output):
     #output = K.clip(output, _EPSILON, 1.0 - _EPSILON)
-    #return -(target * output + (1.0 - target) * (1.0 - output))
-    return K.mean(target*output)
+    return -(target * output + (1.0 - target) * (1.0 - output))
+    #return K.mean(target*output)
 
 def build_generator(latent_size):
     cnn = Sequential()
